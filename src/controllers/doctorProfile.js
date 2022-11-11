1e3701282446f2a4449f67ff948e04cbad52491f
Tahmid Akter <tahmidakter56@gmail.com>
2022-11-11 13:40:44 +0600

    minor fix

--- src/controllers/doctorProfile.js
+++ src/controllers/doctorProfile.js
@@ -1,16 +1,21 @@
-const {PrismaClient} = require("prisma/prisma-client");
+const { PrismaClient } = require("prisma/prisma-client");
 
-const getProfile =async(req,res)=>{
-    let id = req.id;
-    const user = await prisma.user.findUnique({
-        where: {
-          id: id,
-        },
-      })
-    const {pass, ...userData} = foundUser ;
-    res.json({userData});
-}
+const getProfile = async (req, res) => {
+    try {
+        let id = req.id;
+        const prisma = new PrismaClient();
+        const user = await prisma.users.findUnique({
+            where: {
+                id: id,
+            },
+        });
+        const { pass, ...userData } = user;
+        res.json({ userData });
+    } catch (err) {
+        res.json({ Message: err });
+    }
+};
 
-module.exports = { 
-    getProfile
+module.exports = {
+    getProfile,
 };
