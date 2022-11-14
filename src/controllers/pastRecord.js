f2ae5a545f2f07f29458300dc795a589f193d555
AAMasud <amasud183040@bscse.uiu.ac.bd>
2022-11-14 23:46:21 +0600

    time->varchar

--- src/controllers/pastRecord.js
+++ src/controllers/pastRecord.js
@@ -3,7 +3,7 @@ const {query} = require("../helper/db");
 const storeInfo = async (req, res) => {
     // insert into the table
     try{
-        let sqlCommand =  "INSERT INTO `info`(`goalwl`, `motivation`, `targetweight`, `healthproblemshistory`, `bfood`, `btime`, `msnacks`, `mtime`, `lfood`, `ltime`, `esnack`, `etime`, `dfood`, `dtime`, `nsnacks`, `ntime`, `glassesofwater`, `sleeptime`, `sleepTotal`, `exercise`, `currentwt`) VALUES ('"+req.goalwl+"','"+req.motivation+"',"+req.targetweight+",'"+req.healthproblemhistory+"','"+req.bfood+"',"+req.btime+",'"+req.msnacks+"',"+req.mtime+",'"+req.lfood+"',"+req.ltime+",'"+req.esnack+"',"+req.time+",'"+req.dfood+"',"+req.dtime+",'"+req.nsnacks+"',"+req.ntime+",'"+req.glassesofwater+"','"+req.sleeptime+"','"+req.sleeptotal+"','"+req.exercise+"',"+req.currentwt+")"
+        let sqlCommand =  "INSERT INTO `info`(`goalwl`, `motivation`, `targetweight`, `healthproblemshistory`, `bfood`, `btime`, `msnacks`, `mtime`, `lfood`, `ltime`, `esnack`, `etime`, `dfood`, `dtime`, `nsnacks`, `ntime`, `glassesofwater`, `sleeptime`, `sleepTotal`, `exercise`, `currentwt`) VALUES ('"+req.goalwl+"','"+req.motivation+"',"+req.targetweight+",'"+req.healthproblemhistory+"','"+req.bfood+"','"+req.btime+"','"+req.msnacks+"','"+req.mtime+"','"+req.lfood+"','"+req.ltime+"','"+req.esnack+"','"+req.time+"','"+req.dfood+"','"+req.dtime+"','"+req.nsnacks+"','"+req.ntime+"','"+req.glassesofwater+"','"+req.sleeptime+"','"+req.sleeptotal+"','"+req.exercise+"',"+req.currentwt+")"
         let result = await query(sqlCommand);
 
         // get latest record
